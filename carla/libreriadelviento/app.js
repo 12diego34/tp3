06da53d4920218c36579443acfb550b33b2b436f
Your Name <ccsjgs1990@gmail.com>
2016-06-24 15:07:59 -0300

    catalogo lindo

---
+++ carla/libreriadelviento/app.js
@@ -0,0 +1,27 @@
+var db = require('./base');
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+
+
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var books = require('./routes/books');
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/books', books);
+
+module.exports = app;
---
+++ carla/libreriadelviento/public/js/client-app.js
@@ -0,0 +1,38 @@
+angular.module('bookSearchClient',['ngRoute','contollers'])
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.
+        when('/', {
+            templateUrl: 'index',
+            controller: IndexController
+        }).
+        when('/search', {
+            templateUrl: 'books/search',
+            controller: SearchBookController
+        }).
+        when('/show/:id', {
+            templateUrl: 'books/search',
+            controller: ShowBookController
+        }).
+        when('/save', {
+            templateUrl: 'books/search',
+            controller: SaveBookController
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+        $locationProvider.html5Mode(true);
+}])
+
+
+angular.module('LibroApp',['','contollers'])
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.
+        when('/libros/catalogo', {
+            templateUrl: 'index',
+            controller: LibroController
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+        $locationProvider.html5Mode(true);
+}])
